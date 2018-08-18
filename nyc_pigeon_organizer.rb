aadc2c063400e4c3ae316a2d57675cbcefa5ef57
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 23:39:40 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |property, hash|
     hash.each do |attribute, array|
       array.each do |name|
-        if !new_hash.has_key?(name)
+        if !pigeonhash.has_key?(name)
           pigeonash[name] = {}
         end
 
