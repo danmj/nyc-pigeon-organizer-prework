09bcb3d66a0392b5044774752410883b21447b0c
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 23:39:28 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     hash.each do |attribute, array|
       array.each do |name|
         if !new_hash.has_key?(name)
-          new_hash[name] = {}
+          pigeonash[name] = {}
         end
 
         if !new_hash[name].has_key?(property)
